10d7dc79c81d7cd1fdf55243b596704e6b3fec94
Audrey Okumu <okumuaudrey71@gmail.com>
2026-08-06 13:05:30 +0300

    Restructure project into layered architecture (controller, service, repository, model, dto, config, exception)

--- src/main/java/com/atlasassistant/atlasassistant/controller/HomeController.java
+++ src/main/java/com/atlasassistant/atlasassistant/controller/HomeController.java
@@ -1,4 +1,4 @@
-package com.atlasassistant.atlasassistant;
+package com.atlasassistant.atlasassistant.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
