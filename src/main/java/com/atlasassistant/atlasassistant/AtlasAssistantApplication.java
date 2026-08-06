577bdeca5740834699def6b34d22dcb57831a38d
Audrey Okumu <okumuaudrey71@gmail.com>
2026-08-06 12:30:40 +0300

    Initialize Spring Boot project with Spring Web dependency

---
+++ src/main/java/com/atlasassistant/atlasassistant/AtlasAssistantApplication.java
@@ -0,0 +1,13 @@
+package com.atlasassistant.atlasassistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtlasAssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtlasAssistantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/atlasassistant/atlasassistant/AtlasAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atlasassistant.atlasassistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtlasAssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
